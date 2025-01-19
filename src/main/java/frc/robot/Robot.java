e0cec3edbd469ed0ad2f716320f50df246583a5a
tekdemo <tekdemo@gmail.com>
2025-01-19 13:58:23 -0800

    Update dependencies + LaserCan patch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -67,6 +67,8 @@ public class Robot extends LoggedRobot {
     }
 
     Logger.start();
+    au.grapplerobotics.CanBridge.runTCP();
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
