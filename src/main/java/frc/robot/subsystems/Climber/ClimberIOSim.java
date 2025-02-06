5cac861db55019197c5fab2bfce32aa6c2ade5ab
fortierkids1 <fortierkids1@gmail.com>
2025-02-05 18:44:21 -0800

    Spark simulation mostly works

--- src/main/java/frc/robot/subsystems/Climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberIOSim.java
@@ -4,9 +4,16 @@
 
 package frc.robot.subsystems.Climber;
 
+
 import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
@@ -16,17 +23,34 @@ import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 
 /** Add your docs here. */
 public class ClimberIOSim implements ClimberIO {
-  private final DCMotor gearbox;
+  private final DCMotor motorModel;
   private final SparkFlex flex;
   private final SparkFlexSim sim;
   private final SingleJointedArmSim climberSim;
 
   // private double appliedVolts = 0.0;
   public ClimberIOSim() {
-    gearbox = DCMotor.getNeoVortex(1);
+    motorModel = DCMotor.getNeoVortex(1);
     flex = new SparkFlex(9, MotorType.kBrushless);
-    sim = new SparkFlexSim(flex, gearbox);
-    climberSim = new SingleJointedArmSim(gearbox, 360.0, 0.005, 0.5, 0.0, 2*(Math.PI), true, 0.0);
+    sim = new SparkFlexSim(flex, motorModel);
+    climberSim = new SingleJointedArmSim(motorModel, 360.0, SingleJointedArmSim.estimateMOI(.5, 3), 0.5, 0.0, 2*(Math.PI), true, 0.0);
+    
+    var config = new SparkFlexConfig();
+    config.encoder.positionConversionFactor(1);
+    config.absoluteEncoder.positionConversionFactor(360);
+
+    config.inverted(true);
+    config
+        .closedLoop
+        .outputRange(-0.5, 0.5)
+        .p(1 / 30.0)
+        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+        .positionWrappingEnabled(true)
+        // .positionWrappingInputRange(0, 360);
+        .positionWrappingInputRange(-180, 180);
+    config.idleMode(IdleMode.kCoast).smartCurrentLimit(5).voltageCompensation(12.0);
+    config.absoluteEncoder.inverted(false);
+    flex.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
   
     
   }
@@ -37,12 +61,16 @@ public class ClimberIOSim implements ClimberIO {
     climberSim.update(0.02);
     sim.iterate(Units.radiansPerSecondToRotationsPerMinute(climberSim.getVelocityRadPerSec()), RoboRioSim.getVInVoltage(), 0.02);
     RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(climberSim.getCurrentDrawAmps()));
+    inputs.climberAbsoluteAngle = Units.radiansToDegrees(climberSim.getAngleRads());
+    inputs.climberRelativeAngle = Units.radiansToDegrees(climberSim.getAngleRads());
+    inputs.climberCurrentDraw = climberSim.getCurrentDrawAmps();
+    inputs.climberVoltage = sim.getAppliedOutput() * RoboRioSim.getVInVoltage();
     
   }
 
   @Override
   public void setReference(double angle) {
-    
+    flex.getClosedLoopController().setReference(angle, ControlType.kPosition);
   }
 
   // @Override
